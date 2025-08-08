a30288b4ab667aaa05244a657b52e1e46a6c22cd
Miczura <m.gieleta@gmail.com>
2025-08-08 13:03:22 +0200

    changed setup

--- src/test/java/org/example/tests/ValidateRequiredFieldsJobOfferTest.java
+++ src/test/java/org/example/tests/ValidateRequiredFieldsJobOfferTest.java
@@ -1,12 +1,15 @@
 package org.example.tests;
 
 
+import org.example.pages.MainPage;
 import org.testng.annotations.Test;
 
-public class NewDummyTest extends TestBase{
+public class ValidateRequiredFieldsJobOffer extends TestBase{
     @Test
     public void secondDummyTest() throws InterruptedException {
         System.out.println("Running on >>>>> "+Thread.currentThread().getName());
+        MainPage landingPage = new MainPage();
+        landingPage.navigateToBusinessPage();
 
     }
 }
