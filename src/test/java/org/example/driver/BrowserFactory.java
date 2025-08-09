e2a2605bb10c45c8d7e4849b61fcb108d3e7d886
Miczura <m.gieleta@gmail.com>
2025-08-09 14:35:07 +0200

    remove comments

--- src/test/java/org/example/driver/BrowserFactory.java
+++ src/test/java/org/example/driver/BrowserFactory.java
@@ -2,10 +2,9 @@ package org.example.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
+
 
 public class BrowserFactory {
     private static final String MESSAGE_UNKNOWN_BROWSER = "Unknown browser type! Please check your configuration";
@@ -13,16 +12,9 @@ public class BrowserFactory {
     public static WebDriver getBrowser(BrowserType browserType) {
         switch (browserType) {
             case CHROME:
-//                ChromeOptions options = new ChromeOptions();
-//                options.addArguments("--lang=en");
                 return new ChromeDriver();
             case FIREFOX:
 
-//                FirefoxProfile profile = new FirefoxProfile();
-//                profile.setPreference("intl.accept_languages", "en");
-//
-//                FirefoxOptions firefoxOptions = new FirefoxOptions();
-//                firefoxOptions.setProfile(profile);
                 return new FirefoxDriver();
 
             default:
